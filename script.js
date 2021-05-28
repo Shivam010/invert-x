fdf536d1223e5636a3600e2c1753c8271b7c0ec2
Shivam Rathore <shivam.rathore010@gmail.com>
2021-05-29 01:11:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Invert-X is on")
+if (origin.includes("mail.google.com")) {
+    let cnt = 0;
+    const timer = setInterval(function () {
+        let frame = [...document.getElementsByTagName("iframe")].find(c => c.src.includes("tasks.google"));
+        if (frame) {
+            frame.style.filter = "invert(1) brightness(1.2) hue-rotate(175deg)";
+        }
+        if (cnt == 20 || frame) {
+            console.log("Timer closed on", cnt);
+            clearInterval(timer);
+        }
+        cnt++
+    }, 500);
+}
